30b119303fe7755dd26cc9497d59068ff75a3e50
tomastrajan <tomas.trajan@gmail.com>
2024-01-12 01:33:04 +0100

    feat: use route component input binding instead of activated route

--- projects/example-app/src/app/feature/shared-item-editor/item-editor/item-editor.component.ts
+++ projects/example-app/src/app/feature/shared-item-editor/item-editor/item-editor.component.ts
@@ -1,11 +1,9 @@
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 
 import { CardComponent } from '../../../ui/card/card.component';
-import { map } from 'rxjs';
-import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'my-org-item-editor',
@@ -19,9 +17,7 @@ export class ItemEditorComponent {
   private router = inject(Router);
   private activatedRoute = inject(ActivatedRoute);
 
-  name = toSignal(
-    this.activatedRoute.paramMap.pipe(map((params) => params.get('name'))),
-  );
+  name = input<string>();
 
   handleClose() {
     this.router.navigate(['../..'], { relativeTo: this.activatedRoute });
