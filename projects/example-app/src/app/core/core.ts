0366cdaf211de2feb9908dbc25e1112d754b6766
tomastrajan <tomas.trajan@gmail.com>
2024-01-22 17:22:16 +0100

    feat: add inline docs to provide core

--- projects/example-app/src/app/core/core.ts
+++ projects/example-app/src/app/core/core.ts
@@ -29,12 +29,17 @@ export function provideCore({ routes }: CoreOptions) {
       }),
     ),
 
+    // other 3rd party libraries providers like NgRx, provideStore()
+
+    // other application specific providers and setup
+
     // perform initialization, has to be last
     {
       provide: ENVIRONMENT_INITIALIZER,
       multi: true,
       useValue() {
         // add init logic here...
+        // kickstart processes, trigger initial requests or actions, ...
       },
     },
   ];
