da186f9154c2da3866aebfd2ae2f1f957c419b1e
tomastrajan <tomas.trajan@gmail.com>
2024-02-02 11:15:24 +0100

    feat: split shared-feature into shared-feature and shared-feature-routes to enforce desired behavior

--- projects/example-app/src/app/feature/product/product-list/product-list.component.ts
+++ projects/example-app/src/app/feature/product/product-list/product-list.component.ts
@@ -1,12 +1,12 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 
+import { CardComponent } from '../../../ui/card/card.component';
 import { DocumentManagerComponent } from '../../../pattern/document-manager/document-manager.component';
 
 import { ProductService } from '../product.service';
-import { CardComponent } from '../../../ui/card/card.component';
-import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'my-org-product-list',
